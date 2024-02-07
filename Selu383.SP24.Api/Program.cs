ab2ea9e3bedf3eec492b64c7cdaa40a88f5e338f
Bryant Fussell <bryant.fussell@selu.edu>
2024-02-07 11:59:34 -0600

    Added React web

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -48,7 +48,27 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+app
+   .UseRouting()
+   .UseEndpoints(x =>
+   {
+       x.MapControllers();
+   });
+
+ app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(x =>
+    {
+        x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+    });
+}
+else
+{
+    app.MapFallbackToFile("/index.html");
+}
 
 app.Run();
 
